c37ba380a983b99629c604726eb92a803b279690
Darkining__ <treatliam92@gmail.com>
2026-02-13 13:57:06 -0500

    Added date and Dev name

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -1,6 +1,8 @@
 /*
   Blink
 
+Dev: Liam Treat
+Date: 2/13/2026
   Turns an LED on for one second, then off for one second, repeatedly.
 
   Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
