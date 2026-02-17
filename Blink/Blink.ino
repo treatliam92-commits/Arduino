0c1c46b24e2cbf7cee97e946fef85db630f9e28f
Darkining__ <treatliam92@gmail.com>
2026-02-17 13:47:12 -0500

    changed pin output to 10

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -28,13 +28,13 @@ Date: 2/13/2026
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(10, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(10, HIGH);  // turn the LED on (HIGH is the voltage level)
   delay(3000);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  digitalWrite(10, LOW);   // turn the LED off by making the voltage LOW
   delay(2000);                      // wait for a second
 }
