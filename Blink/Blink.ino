3c3e4e92d7af15d9d12760a424faf036c18dc6ef
Darkining__ <treatliam92@gmail.com>
2026-02-18 12:41:53 -0500

    added police siren

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -26,19 +26,28 @@ Date: 2/13/2026
 */
 
 // the setup function runs once when you press reset or power the board
+int buzzer = 6;  // Pin the piezo is connected to
+
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
   pinMode(10, OUTPUT);
   pinMode(9, OUTPUT);
+  pinMode(buzzer, OUTPUT);
 }
 
-
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(10, HIGH);  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(9, LOW);  // turn the LED on (HIGH is the voltage level)
-  delay(250);                      // wait for a second
-   digitalWrite(10, LOW);   // turn the LED off by making the voltage LOW
-  digitalWrite(9, HIGH);   // turn the LED off by making the voltage LOW
-  delay(250);                      // wait for a second
+  digitalWrite(10, HIGH);
+  digitalWrite(9, LOW);
+  delay(250);
+
+  digitalWrite(10, LOW);
+  digitalWrite(9, HIGH);
+  delay(250);
+
+  tone(buzzer, 1000);  // Play 1000 Hz
+  delay(250);
+
+  tone(buzzer, 400);   // Play 400 Hz
+  delay(250);
 }
+
+
